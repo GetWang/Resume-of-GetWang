09b577f98677ad59b986eb75e52b5984d151dbd3
GetWang <wang_dresimap@qq.com>
2021-03-19 00:00:17 +0800

    update online link

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const { src, dest, watch, series } = require("gulp");
 const less = require("gulp-less");
 
-/* 编译less文件 */
+/* 编译 less 文件 */
 function compileLess(cb) {
   src("src/less/*.less").pipe(less()).pipe(dest("src/css"));
   cb();
 }
 
-/* 监听less文件的变动 */
+/* 监听 less 文件的变动 */
 function lessWatch(cb) {
   watch(["src/less/*.less"], compileLess);
   cb();
